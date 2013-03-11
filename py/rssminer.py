8e37d391e008fc88efdd464fbc10c3e9a489206b
tensorjack <jack@tinybike.net>
2013-03-11 01:55:28 -0400

    RSS miner fixes

--- py/rssminer.py
+++ py/rssminer.py
@@ -8,6 +8,7 @@ RSS article data miner.
 from __future__ import division
 import MySQLdb
 import re
+import sys
 
 def main():
 	# Set up topic-word association lists
@@ -26,42 +27,75 @@ def main():
 	cur.connection.autocommit(True)
 
 	# Get city names and grid coordinates from database
+	# For testing, limit to U.S. cities
+	search_city = 'Los Angeles'
+	print 'Fetching test country code for ' + search_city + '...'
+	sql = (
+		'SELECT DISTINCT country_id FROM cities '
+		'WHERE name = "' + search_city + '";'
+	)
+	cur.execute(sql)
+	country_id = cur.fetchall()[0][0]
+	
+	# Need to get rid of small-pop cities with redundant names...
+	print 'Fetching all cities with country code ' + str(country_id) + '...'
 	city = {}
-	cur.execute('SELECT id, name FROM cities;')
+	sql = (
+		'SELECT id, name FROM cities '
+		'WHERE country_id = ' + str(country_id) + ' AND '
+		'(name = "Los Angeles" OR name = "San Francisco"'
+		'OR name = "Pensacola" OR name = "Albuquerque"'
+		'OR name = "New Orleans" OR name = "Kansas City");'
+	)
+	cur.execute(sql)
+	print str(cur.rowcount) + ' cities found.'
 	for row in cur.fetchall():
 		city[row[0]] = {'name': row[1], 'sick_words': 0}
 	
 	# Fetch all RSS-linked articles from database which were updated
-	# within the last 3 days
+	# within the last 5 days
+	print 'Fetching all articles updated in the last 5 days...'
 	sql = (
-		'SELECT article FROM rss_data '
-		'WHERE updated > DATE_SUB(NOW(), INTERVAL 3 DAY);'
+		'SELECT title, article FROM rss_data '
+		'WHERE updated > DATE_SUB(NOW(), INTERVAL 5 DAY);'
 	)
 	cur.execute(sql)
 	num_articles = cur.rowcount
-	article = [a[0] for a in cur.fetchall()]
+	sql_results = cur.fetchall()
+	title = [row[0] for row in sql_results]
+	article = [row[1] for row in sql_results]
 
 	# Set up one big regex for sick words
+	print 'Setting up regex...'
 	sick_patterns = [r'\b%s\b' % re.escape(s.strip()) for s in sick_words]
 	sick_channel = re.compile('|'.join(sick_patterns))
 
 	# Find the occurrences of sick words and city names in each article
+	print 'Analyzing article text...'
 	for i, a in enumerate(article):
+		# Visual progress indicator
+		sys.stdout.write('.')
+		if not i % 10:
+			print
+
 		# Clean up article text a little
 		text = a.replace('\t', '').replace('\n', '').replace('\r', '')
-
-		# Find and count up any sick words in the article
-		num_sick_words = len(re.findall(sick_channel, text.lower()))
-	
-		# Increment the sick word counts of any cities with their
-		# names found in the article
+		
+		# Search the article for city names
 		for city_id in city.keys():
 			city_pattern = r'\b%s\b' % re.escape(city[city_id]['name'].strip())
 			city_found = re.findall(city_pattern, text)
-			if city_found:
-				city[city_id]['sick_words'] += num_sick_words
+			if city_found and city_found[0]:
+				# Increment the sick word counts of any cities with their
+				# names found in the article
+				num_sick_words = len(re.findall(sick_channel, text.lower()))
+				if num_sick_words:
+					print city[city_id]['name'] + ': ' + str(num_sick_words)
+					city[city_id]['sick_words'] += num_sick_words
 		
+	
 	# Write results to database
+	print 'Writing results to database...'
 	for city_id in city.keys():
 		sql = (
 			'INSERT INTO city_sick_counts VALUES (%i, %i);' 
@@ -72,6 +106,7 @@ def main():
 	# Close database
 	cur.close()
 	db.close()
+	print 'Done.'
 	
 if __name__ == '__main__':
 	main()
