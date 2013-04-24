b359b3f362bd382bfb10de2e482795da2cca1b69
Jack Peterson <jack@tinybike.net>
2013-04-24 16:05:36 -0700

    'wrapper'

---
+++ py/rsswrap.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+RSS mining wrapper.
+
+(c) Jack Peterson (jack@tinybike.net), 4/24/2013
+"""
+
+from rssparse import rssparse
+from rssminer import rssminer
+
+rssparse()
+rssminer()
